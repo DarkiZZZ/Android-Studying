99f56938df2824cc32e4551aaca904f025296835
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-06-13 01:06:39 +0300

    got sample of ViewModel from provider

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,31 +1,27 @@
 package com.example.photogallery
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment(){
 
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchContents()
-        flickrLiveData.observe(this, Observer {
-            galleryItems ->
-            Log.d(TAG, "Response received: $galleryItems")
-        })
+        photoGalleryViewModel =
+            ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
     }
-
 
 
     override fun onCreateView(
