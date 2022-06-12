fc7fa83092eb3598ee5acdae8bb0a7520a2ad89d
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-06-13 00:59:26 +0300

    added viewModel for List<GalleryItem>

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.photogallery
 
-class PhotoGalleryViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel(){
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchContents()
+    }
 }
