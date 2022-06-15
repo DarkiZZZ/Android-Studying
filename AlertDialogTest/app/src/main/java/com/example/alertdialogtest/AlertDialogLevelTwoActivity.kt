7dc55efd3444c9032307cb87435920ce90c42a72
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-15 16:26:21 +0300

    init commit

---
+++ AlertDialogTest/app/src/main/java/com/example/alertdialogtest/AlertDialogLevelTwoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alertdialogtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlertDialogLevelTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_dialog_level_two)
+    }
+}
