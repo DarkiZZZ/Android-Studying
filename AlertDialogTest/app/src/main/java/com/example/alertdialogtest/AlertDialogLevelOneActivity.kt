9645d5a85770093dfd574680d38f047b286a0006
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-15 16:22:56 +0300

    renamed

--- AlertDialogTest/app/src/main/java/com/example/alertdialogtest/AlertDialogLevelOneActivity.kt
+++ AlertDialogTest/app/src/main/java/com/example/alertdialogtest/AlertDialogLevelOneActivity.kt
@@ -3,7 +3,7 @@ package com.example.alertdialogtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AlertDialogLevelOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
