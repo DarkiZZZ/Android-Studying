dca01eeb5712f012f41d3fa82dc4012b2912eeeb
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-22 17:59:41 +0300

    completed third alert dialog

--- AlertDialogTest/app/src/main/java/com/example/alertdialogtest/AlertDialogLevelTwoActivity.kt
+++ AlertDialogTest/app/src/main/java/com/example/alertdialogtest/AlertDialogLevelTwoActivity.kt
@@ -1,10 +1,15 @@
 package com.example.alertdialogtest
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AlertDialog
 import com.example.alertdialogtest.databinding.ActivityAlertDialogLevelTwoBinding
 import com.example.alertdialogtest.databinding.PartVolumeBinding
+import com.example.alertdialogtest.databinding.PartVolumeInputBinding
 import com.example.alertdialogtest.entities.AvailableVolumeValues
 import kotlin.properties.Delegates
 
@@ -25,6 +30,9 @@ class AlertDialogLevelTwoActivity : AppCompatActivity() {
         binding.alertDialogCustomViewButton.setOnClickListener { showCustomAlertDialog() }
         binding.alertDialogSingleChoiceCustomButton.setOnClickListener {
             showCustomSingleChoiceAlertDialog()
+        }
+        binding.alertDialogInputAndValidationButton.setOnClickListener {
+            showCustomInputAlertDialog()
         }
         updateUi()
     }
@@ -77,5 +85,52 @@ class AlertDialogLevelTwoActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    private fun showCustomInputAlertDialog(){
+        val dialogBinding = PartVolumeInputBinding.inflate(layoutInflater)
+        dialogBinding.volumeInputEditText.setText(volume.toString())
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.enter_volume))
+            .setView(dialogBinding.root)
+            .setPositiveButton("Confirm", null)
+            .create()
+        dialog.setOnShowListener {
+            dialogBinding.volumeInputEditText.requestFocus()
+            showKeyboard(dialogBinding.volumeInputEditText)
+
+            dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+                val enteredText = dialogBinding.volumeInputEditText.text.toString()
+                if (enteredText.isBlank()){
+                    dialogBinding.volumeInputEditText.error = "Value is empty"
+                    return@setOnClickListener
+                }
+                val volume: Int? = enteredText.toIntOrNull()
+                if (volume == null || volume > 100){
+                    dialogBinding.volumeInputEditText.error = "Invalid value"
+                    return@setOnClickListener
+                }
+                this.volume = volume
+                updateUi()
+                dialog.dismiss()
+            }
+        }
+        dialog.setOnDismissListener { hideKeyboard(dialogBinding.volumeInputEditText) }
+        dialog.show()
+    }
+
+    private fun hideKeyboard(view: View){
+        getInputMethodManager(view).showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun showKeyboard(view: View){
+        view.post {
+            getInputMethodManager(view).hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun getInputMethodManager(view: View): InputMethodManager{
+        val context: Context = view.context
+        return context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
 }
