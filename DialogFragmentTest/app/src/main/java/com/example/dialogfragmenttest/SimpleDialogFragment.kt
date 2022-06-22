b6576ac95871a1f0b86137c859014b4dc2656249
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-06-22 22:26:46 +0300

    added simple dialog fragment

--- DialogFragmentTest/app/src/main/java/com/example/dialogfragmenttest/SimpleDialogFragment.kt
+++ DialogFragmentTest/app/src/main/java/com/example/dialogfragmenttest/SimpleDialogFragment.kt
@@ -1,4 +1,34 @@
 package com.example.dialogfragmenttest
 
-class SimpleDialogFragment {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
+
+
+class SimpleDialogFragment : DialogFragment() {
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val listener = DialogInterface.OnClickListener { _, which ->
+             parentFragmentManager.setFragmentResult(REQUEST_KEY, bundleOf(KEY_RESPONSE to which))
+        }
+        return AlertDialog.Builder(requireContext())
+            .setCancelable(true)
+            .setIcon(R.mipmap.ic_launcher_round)
+            .setTitle("AlertDialog Title")
+            .setMessage("Test Question")
+            .setPositiveButton("Yes", listener)
+            .setNegativeButton("No", listener)
+            .setNeutralButton("Ignore", listener)
+            .create()
+    }
+
+    companion object{
+        @JvmStatic val TAG = SimpleDialogFragment::class.java.simpleName
+        @JvmStatic val REQUEST_KEY = "$TAG: defaultRequestKey"
+        @JvmStatic val KEY_RESPONSE = "RESPONSE"
+    }
 }
