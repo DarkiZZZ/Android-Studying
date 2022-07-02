fde84bde01c02f18dd58fbf8f32da5dce5abb62f
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-02 23:50:48 +0300

    init commit

---
+++ app/src/main/java/com/example/handlertestapp/HandlerLevelTwoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.handlertestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HandlerLevelTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_handler_level_two)
+    }
+}
