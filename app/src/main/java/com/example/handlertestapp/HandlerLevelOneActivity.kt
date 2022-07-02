4ebf0666b179079b4585bd1cc22fd3cf7cf2036f
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-03 00:04:53 +0300

    prepared handler activities

--- app/src/main/java/com/example/handlertestapp/HandlerLevelOneActivity.kt
+++ app/src/main/java/com/example/handlertestapp/HandlerLevelOneActivity.kt
@@ -2,10 +2,21 @@ package com.example.handlertestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.handlertestapp.databinding.ActivityHandlerLevelOneBinding
 
 class HandlerLevelOneActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHandlerLevelOneBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_handler_level_one)
+        binding = ActivityHandlerLevelOneBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+
+    override fun onNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/handlertestapp/HandlerLevelTwoActivity.kt
+++ app/src/main/java/com/example/handlertestapp/HandlerLevelTwoActivity.kt
@@ -2,10 +2,21 @@ package com.example.handlertestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.handlertestapp.databinding.ActivityHandlerLevelTwoBinding
 
 class HandlerLevelTwoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHandlerLevelTwoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_handler_level_two)
+        binding = ActivityHandlerLevelTwoBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+
+    override fun onNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
