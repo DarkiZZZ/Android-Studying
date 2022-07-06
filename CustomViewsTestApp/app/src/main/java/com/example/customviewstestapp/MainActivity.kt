1e8660a35ee041282d02e537a1c52a058a9ba4de
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-06 18:08:27 +0300

    tested listeners and attributes

--- CustomViewsTestApp/app/src/main/java/com/example/customviewstestapp/MainActivity.kt
+++ CustomViewsTestApp/app/src/main/java/com/example/customviewstestapp/MainActivity.kt
@@ -1,25 +1,45 @@
 package com.example.customviewstestapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.example.customviewstestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val token = Any()
+    private val handler = Handler(Looper.getMainLooper())
+
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        with(binding.bottomButtons){
-            applyButton.text  = getString(R.string.ok)
-            applyButton.setOnClickListener {
-                progressBar.visibility = View.VISIBLE
-                cancelButton.visibility = View.INVISIBLE
-                applyButton.visibility = View.INVISIBLE
+
+        binding.bottomButtons.setListener {
+            when(it){
+                BottomButtonAction.POSITIVE -> {
+                    binding.bottomButtons.isProgressMode = true
+                    handler.postDelayed({
+                        binding.bottomButtons.isProgressMode = false
+                        binding.bottomButtons.setApplyButtonText("Confirmation")
+                    }, token, 2000L)
+                }
+
+                BottomButtonAction.NEGATIVE ->{
+                    binding.bottomButtons.setCancelButtonText("Rejection")
+                }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(token)
     }
 }
