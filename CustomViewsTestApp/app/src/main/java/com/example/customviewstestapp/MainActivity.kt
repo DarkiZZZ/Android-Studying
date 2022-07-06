6ab34bdf5e6ea5bb4955ffab2d184e58afd28363
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-06 10:37:18 +0300

    added include to main.xml and simple logic

--- CustomViewsTestApp/app/src/main/java/com/example/customviewstestapp/MainActivity.kt
+++ CustomViewsTestApp/app/src/main/java/com/example/customviewstestapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.customviewstestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.customviewstestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        with(binding.bottomButtons){
+            applyButton.text  = getString(R.string.ok)
+            applyButton.setOnClickListener {
+                progressBar.visibility = View.VISIBLE
+                cancelButton.visibility = View.INVISIBLE
+                applyButton.visibility = View.INVISIBLE
+            }
+        }
     }
 }
