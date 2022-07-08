7ff8180957f12d2f08c1b0dbf7e32e36a29b25c3
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-08 15:38:38 +0300

    initialized attrs' colors

--- TicTacToeCustomViewApp/app/src/main/java/com/example/tictactoecustomviewapp/TicTacToeView.kt
+++ TicTacToeCustomViewApp/app/src/main/java/com/example/tictactoecustomviewapp/TicTacToeView.kt
@@ -1,14 +1,20 @@
 package com.example.tictactoecustomviewapp
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
+import kotlin.properties.Delegates
 
 class TicTacToeView(
     context: Context,
     attributeSet: AttributeSet?,
     defStyleAttr: Int,
     defStyleRes: Int) : View(context, attributeSet, defStyleAttr, defStyleRes) {
+
+    private var crossColor: Int by Delegates.notNull<Int>()
+    private var zeroColor: Int by Delegates.notNull<Int>()
+    private var gridColor: Int by Delegates.notNull<Int>()
 
     constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) :
             this(context, attributeSet, defStyleAttr, R.style.DefaultTicTacToeStyle)
@@ -26,6 +32,28 @@ class TicTacToeView(
     private fun initializeAttributeSet(attributeSet: AttributeSet?,
                                        defStyleAttr: Int,
                                        defStyleRes: Int){
+        val typedArray = context.obtainStyledAttributes(
+            attributeSet,
+            R.styleable.TicTacToeView,
+            defStyleAttr,
+            defStyleRes)
+        crossColor = typedArray.getColor(R.styleable.TicTacToeView_crossColor, CROSS_DEFAULT_COLOR)
+        zeroColor = typedArray.getColor(R.styleable.TicTacToeView_zeroColor, ZERO_DEFAULT_COLOR)
+        gridColor = typedArray.getColor(R.styleable.TicTacToeView_gridColor, GRID_DEFAULT_COLOR)
 
+
+        typedArray.recycle()
+    }
+
+    private fun initializeDefaultColors(){
+        crossColor = CROSS_DEFAULT_COLOR
+        zeroColor = ZERO_DEFAULT_COLOR
+        gridColor = GRID_DEFAULT_COLOR
+    }
+
+    companion object{
+        private const val CROSS_DEFAULT_COLOR = Color.BLUE
+        private const val ZERO_DEFAULT_COLOR = Color.RED
+        private const val GRID_DEFAULT_COLOR = Color.GRAY
     }
 }
