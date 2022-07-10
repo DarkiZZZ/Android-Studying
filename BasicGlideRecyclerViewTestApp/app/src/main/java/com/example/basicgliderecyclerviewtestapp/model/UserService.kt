2bc7e7a31250b255e5febc754e3e3fe3ec795631
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-10 20:59:43 +0300

    added listeners and funs to manage them

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/model/UserService.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/model/UserService.kt
@@ -3,9 +3,13 @@ package com.example.basicgliderecyclerviewtestapp.model
 import com.github.javafaker.Faker
 import java.util.*
 
+typealias UsersListener = (users: List<User>) -> Unit
+
 class UserService {
 
     private var users = mutableListOf<User>()
+    private val listeners = mutableSetOf<UsersListener>()
+
     init {
         val faker = Faker.instance()
         IMAGES_URL_LIST.shuffle()
@@ -26,15 +30,32 @@ class UserService {
         if (deleteIndex != -1){
             users.removeAt(deleteIndex)
         }
+        notifyChanges()
     }
-                    // relocation shows what direction you need to
-                    // relocate user: 1(up), -1(down), 0(stay)
+
+
+    // relocation shows what direction you need to
+    // relocate user: 1(up), -1(down), 0(stay)
     fun relocateUser(user: User, relocation: Int){
         val oldIndex = users.indexOfFirst { it.id == user.id }
         val newIndex = oldIndex + relocation
         if (newIndex < 0 || newIndex >= users.size){
             Collections.swap(users, oldIndex, newIndex)
         }
+        notifyChanges()
+    }
+
+    fun addListener(listener: UsersListener){
+        listeners.add(listener)
+        listener.invoke(users)
+    }
+
+    fun removeListener(listener: UsersListener){
+        listeners.remove(listener)
+    }
+
+    private fun notifyChanges(){
+        listeners.forEach { it.invoke(users) }
     }
 
     companion object{
