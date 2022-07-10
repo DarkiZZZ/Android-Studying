37b29b73f5beb2c53b868b1e32a970ff46f429d7
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-10 20:28:58 +0300

    added logic of Users init block

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/model/UserService.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/model/UserService.kt
@@ -1,4 +1,23 @@
 package com.example.basicgliderecyclerviewtestapp.model
 
+import com.github.javafaker.Faker
+
 class UserService {
+
+    private var users = mutableListOf<User>()
+    init {
+        val faker = Faker.instance()
+        IMAGES_URL_LIST.shuffle()
+        val createdUsers = (1..100).map { User(
+            id = it.toLong(),
+            name = faker.name().fullName(),
+            company = faker.company().name(),
+            photo = IMAGES_URL_LIST[it % IMAGES_URL_LIST.size]
+        ) }
+
+    }
+
+    companion object{
+        private val IMAGES_URL_LIST = mutableListOf<String>()
+    }
 }
