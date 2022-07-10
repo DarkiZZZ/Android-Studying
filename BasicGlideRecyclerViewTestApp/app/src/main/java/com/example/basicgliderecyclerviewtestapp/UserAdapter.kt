b86f5647afd5937273f4830785d86497f52a4478
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-10 22:04:57 +0300

    fulled adapter

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/UserAdapter.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/UserAdapter.kt
@@ -1,24 +1,47 @@
 package com.example.basicgliderecyclerviewtestapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.basicgliderecyclerviewtestapp.databinding.ItemUserBinding
+import com.example.basicgliderecyclerviewtestapp.model.User
 
 class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
+    var users : List<User> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
     class UserViewHolder(val binding: ItemUserBinding)
-        : RecyclerView.ViewHolder(binding.root){
-    }
+        : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemUserBinding.inflate(inflater, parent, false)
+        return UserViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = users[position]
+        with(holder.binding){
+            userNameTextView.text = user.name
+            userCompanyTextView.text = user.company
+            if (user.photo.isNotBlank()){
+                Glide.with(userPhotoImageView.context)
+                    .load(user.photo)
+                    .circleCrop()
+                    .placeholder(R.drawable.ic_user_photo)
+                    .error(R.drawable.ic_user_photo)
+                    .into(userPhotoImageView)
+            }
+            else{
+                userPhotoImageView.setImageResource(R.drawable.ic_user_photo)
+            }
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = users.size
 }
