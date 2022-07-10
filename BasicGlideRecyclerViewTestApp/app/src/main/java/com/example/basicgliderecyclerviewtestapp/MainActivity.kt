9c55106fddbb51a8700ea9c4d1b3c91115ac1baa
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-10 22:19:04 +0300

    added recycler view to Main Activity

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/MainActivity.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.basicgliderecyclerviewtestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.basicgliderecyclerviewtestapp.databinding.ActivityMainBinding
+import com.example.basicgliderecyclerviewtestapp.model.UserService
+import com.example.basicgliderecyclerviewtestapp.model.UsersListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: UserAdapter
+    private val userService: UserService
+        get() = (applicationContext as UserApp).usersService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        adapter = UserAdapter()
+        val layoutManager =  LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        userService.addListener { usersListener }
+    }
+
+    private val usersListener: UsersListener = {
+        adapter.users = it
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        userService.removeListener(usersListener)
     }
 }
