70753b105083a290c70f0a9ebf4eccc76fbed1b5
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-12 22:53:04 +0300

    added viewmodel and observer to fragment

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListFragment.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.basicgliderecyclerviewtestapp.UserActionListener
 import com.example.basicgliderecyclerviewtestapp.UserAdapter
@@ -17,6 +19,8 @@ class UserListFragment : Fragment() {
     private lateinit var binding: FragmentUserListBinding
     private lateinit var adapter: UserAdapter
 
+    private val viewModel: UserListViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,17 +29,21 @@ class UserListFragment : Fragment() {
         binding = FragmentUserListBinding.inflate(layoutInflater, container, false)
         adapter = UserAdapter(object : UserActionListener {
             override fun onUserRelocate(user: User, relocation: Int) {
-                TODO("Not yet implemented")
+                viewModel.relocateUser(user, relocation)
             }
 
             override fun onUserDelete(user: User) {
-                TODO("Not yet implemented")
+                viewModel.deleteUser(user)
             }
 
             override fun onUserDetails(user: User) {
                 TODO("Not yet implemented")
             }
 
+        })
+
+        viewModel.users.observe(viewLifecycleOwner, Observer {
+            adapter.users = it
         })
 
         val layoutManager = LinearLayoutManager(requireContext())
