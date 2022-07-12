4cdf20d4d50d95e01a50ef1f3f5299657e5b1a2a
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-12 22:47:00 +0300

    added ViewModel for UserListViewModel

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListViewModel.kt
@@ -1,6 +1,38 @@
 package com.example.basicgliderecyclerviewtestapp.screens
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.basicgliderecyclerviewtestapp.model.User
+import com.example.basicgliderecyclerviewtestapp.model.UserService
+import com.example.basicgliderecyclerviewtestapp.model.UsersListener
 
-class UserListViewModel : ViewModel() {
+class UserListViewModel(private val userService: UserService) : ViewModel() {
+
+    private val viewModelUsers = MutableLiveData<List<User>>()
+    var users: LiveData<List<User>> = viewModelUsers
+
+    init {
+        loadUsers()
+    }
+    private val listener: UsersListener = {
+        viewModelUsers.value = it
+    }
+    fun loadUsers(){
+        userService.addListener { listener }
+    }
+
+    fun relocateUser(user: User, relocation: Int){
+        userService.relocateUser(user, relocation)
+    }
+
+    fun deleteUser(user: User){
+        userService.deleteUser(user)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        userService.removeListener(listener)
+    }
+
 }
