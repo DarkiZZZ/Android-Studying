2f9714d14d4da0ef8e0718b4a668a7aab1e7538a
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-12 22:46:38 +0300

    prepared fragment userList

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListFragment.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserListFragment.kt
@@ -1,8 +1,47 @@
 package com.example.basicgliderecyclerviewtestapp.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.basicgliderecyclerviewtestapp.UserActionListener
+import com.example.basicgliderecyclerviewtestapp.UserAdapter
+import com.example.basicgliderecyclerviewtestapp.databinding.FragmentUserListBinding
+import com.example.basicgliderecyclerviewtestapp.model.User
 
 
 class UserListFragment : Fragment() {
 
+    private lateinit var binding: FragmentUserListBinding
+    private lateinit var adapter: UserAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserListBinding.inflate(layoutInflater, container, false)
+        adapter = UserAdapter(object : UserActionListener {
+            override fun onUserRelocate(user: User, relocation: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onUserDelete(user: User) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onUserDetails(user: User) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        return binding.root
+    }
 }
