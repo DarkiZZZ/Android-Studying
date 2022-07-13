eb437eaf2f55c50538b206b6ee06af018cb38ddb
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-13 19:02:23 +0300

    implemented interface Navigator

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/MainActivity.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.basicgliderecyclerviewtestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.basicgliderecyclerviewtestapp.databinding.ActivityMainBinding
 import com.example.basicgliderecyclerviewtestapp.model.User
 import com.example.basicgliderecyclerviewtestapp.model.UserService
 import com.example.basicgliderecyclerviewtestapp.model.UsersListener
+import com.example.basicgliderecyclerviewtestapp.screens.Navigator
+import com.example.basicgliderecyclerviewtestapp.screens.UserDetailsFragment
 import com.example.basicgliderecyclerviewtestapp.screens.UserListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator{
 
     private lateinit var binding: ActivityMainBinding
 
@@ -25,6 +28,20 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragmentContainer, UserListFragment())
                 .commit()
         }
+    }
+
+    override fun showDetails(user: User) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, UserDetailsFragment.newInstance(user.id))
+            .commit()
+    }
+
+    override fun goBack() {
+        onBackPressed()
+    }
+
+    override fun showToast(messageRes: Int) {
+        Toast.makeText(this, messageRes, Toast.LENGTH_SHORT).show()
     }
 
 
