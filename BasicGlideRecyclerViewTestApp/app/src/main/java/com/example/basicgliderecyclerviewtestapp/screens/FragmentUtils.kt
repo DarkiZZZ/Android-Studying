2905b277df508fc547b44a60951ec2aabf4a14d4
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-13 12:20:49 +0300

    added viewmodel factory

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/FragmentUtils.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/FragmentUtils.kt
@@ -1,2 +1,24 @@
 package com.example.basicgliderecyclerviewtestapp.screens
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.basicgliderecyclerviewtestapp.UserApp
+
+class ViewModelFactory(private val userApp: UserApp) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        val viewModel: UserListViewModel = when(modelClass){
+
+            UserListViewModel::class.java -> {
+                UserListViewModel(userApp.usersService)
+            }
+
+            else -> throw IllegalStateException("Unknown viewModel class")
+        }
+
+        return viewModel as T
+    }
+
+}
+
+fun Fragment.factory()  = ViewModelFactory(requireContext().applicationContext as UserApp)
