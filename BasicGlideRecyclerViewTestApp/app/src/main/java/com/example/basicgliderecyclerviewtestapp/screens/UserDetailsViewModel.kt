37bf33509655cf7c77434fdb44c492734fd55440
DarkiZZZ <mark_sokolov_2016@mail.ru>
2022-07-13 18:57:46 +0300

    added viewModel to UserDetailsFragment

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.basicgliderecyclerviewtestapp.screens
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.basicgliderecyclerviewtestapp.UserNotFoundException
+import com.example.basicgliderecyclerviewtestapp.model.UserDetails
+import com.example.basicgliderecyclerviewtestapp.model.UserService
 
-class UserDetailsViewModel : ViewModel() {
+class UserDetailsViewModel(private val userService: UserService) : ViewModel() {
+
+    private val viewModelUserDetails = MutableLiveData<UserDetails>()
+    val userDetails: LiveData<UserDetails> = viewModelUserDetails
+
+    fun loadUsers(userId: Long){
+        if(viewModelUserDetails.value != null) return
+        try {
+            viewModelUserDetails.value = userService.getUserDetailsById(userId)
+        }
+        catch (e: UserNotFoundException){
+            e.stackTrace
+        }
+
+    }
+
+    fun deleteUser(){
+        val userDetails: UserDetails = this.userDetails.value ?: return
+        userService.deleteUser(userDetails.user)
+    }
 }
