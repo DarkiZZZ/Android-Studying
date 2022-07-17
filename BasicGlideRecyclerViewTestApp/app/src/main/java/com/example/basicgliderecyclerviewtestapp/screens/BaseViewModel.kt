adef9a8e5bda6418ae094a7860298f30fa038fa1
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-17 23:03:37 +0300

    added Event(singleLiveData analog) for showing toasts

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/BaseViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/BaseViewModel.kt
@@ -3,6 +3,19 @@ package com.example.basicgliderecyclerviewtestapp.screens
 import androidx.lifecycle.ViewModel
 import com.example.basicgliderecyclerviewtestapp.tasks.UserTask
 
+class Event<T>(
+    private val value: T
+){
+
+    private var handled: Boolean = false
+
+    fun getValue(): T? {
+        if (handled) return null
+        handled = true
+        return value
+    }
+}
+
 open class BaseViewModel : ViewModel() {
 
     private val tasks = mutableListOf<UserTask<*>>()
