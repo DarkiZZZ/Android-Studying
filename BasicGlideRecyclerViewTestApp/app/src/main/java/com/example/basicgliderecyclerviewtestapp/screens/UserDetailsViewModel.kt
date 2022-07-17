da783229598aece3ea7505e3d7d637087e306e1e
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-17 22:58:33 +0300

    added support of async tasks

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
@@ -2,29 +2,62 @@ package com.example.basicgliderecyclerviewtestapp.screens
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.basicgliderecyclerviewtestapp.UserNotFoundException
 import com.example.basicgliderecyclerviewtestapp.model.UserDetails
 import com.example.basicgliderecyclerviewtestapp.model.UserService
+import com.example.basicgliderecyclerviewtestapp.tasks.EmptyResult
+import com.example.basicgliderecyclerviewtestapp.tasks.PendingResult
+import com.example.basicgliderecyclerviewtestapp.tasks.SuccessResult
+import com.example.basicgliderecyclerviewtestapp.tasks.UserResult
 
-class UserDetailsViewModel(private val userService: UserService) : ViewModel() {
+class UserDetailsViewModel(private val userService: UserService) : BaseViewModel() {
 
-    private val viewModelUserDetails = MutableLiveData<UserDetails>()
-    val userDetails: LiveData<UserDetails> = viewModelUserDetails
+    private val viewModelState = MutableLiveData<State>()
+    val state: LiveData<State> = viewModelState
+
+    private val currentState: State get() = state.value!!
+
+    init {
+        viewModelState.value = State(
+            userDetailsResult = EmptyResult(),
+            deletingInProgress = false
+        )
+    }
 
     fun loadUsers(userId: Long){
-        if(viewModelUserDetails.value != null) return
-        try {
-            viewModelUserDetails.value = userService.getUserDetailsById(userId)
-        }
-        catch (e: UserNotFoundException){
-            e.stackTrace
-        }
+        if (currentState.userDetailsResult is SuccessResult) return
 
+        viewModelState.value = currentState.copy(userDetailsResult = PendingResult())
+        userService.getUserDetailsById(userId)
+            .onSuccess {
+                viewModelState.value = currentState.copy(userDetailsResult = SuccessResult(it))
+            }
+            .onError {  }
+            .autoCancel()
     }
 
     fun deleteUser(){
-        val userDetails: UserDetails = this.userDetails.value ?: return
-        userService.deleteUser(userDetails.user)
+        val userDetailsResult = currentState.userDetailsResult
+        if (userDetailsResult !is SuccessResult) return
+        viewModelState.value = currentState.copy(deletingInProgress = true)
+        userService.deleteUser(userDetailsResult.data.user)
+            .onSuccess {
+                //todo
+            }
+            .onError {
+                viewModelState.value = currentState.copy(deletingInProgress = false)
+            }
+    }
+
+
+    data class State(
+        val userDetailsResult: UserResult<UserDetails>,
+        private val deletingInProgress: Boolean
+    ){
+
+        val showContent: Boolean get() = userDetailsResult is SuccessResult
+        val showProgress: Boolean get() = userDetailsResult is PendingResult || deletingInProgress
+        val enableDeleteButton: Boolean get() = !deletingInProgress
     }
 }
+
