c309a390d33be0175c657f49f668325c49483175
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-17 23:12:27 +0300

    added events to show toasts

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/UserDetailsViewModel.kt
@@ -2,6 +2,7 @@ package com.example.basicgliderecyclerviewtestapp.screens
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.basicgliderecyclerviewtestapp.R
 import com.example.basicgliderecyclerviewtestapp.UserNotFoundException
 import com.example.basicgliderecyclerviewtestapp.model.UserDetails
 import com.example.basicgliderecyclerviewtestapp.model.UserService
@@ -14,6 +15,15 @@ class UserDetailsViewModel(private val userService: UserService) : BaseViewModel
 
     private val viewModelState = MutableLiveData<State>()
     val state: LiveData<State> = viewModelState
+
+
+
+    private val viewModelActionShowToast = MutableLiveData<Event<Int>>()
+    val actionShowToast: LiveData<Event<Int>> = viewModelActionShowToast
+
+    private val viewModelActionGoBack = MutableLiveData<Event<Unit>>()
+    val actionGoBack: LiveData<Event<Unit>> = viewModelActionGoBack
+
 
     private val currentState: State get() = state.value!!
 
@@ -32,7 +42,10 @@ class UserDetailsViewModel(private val userService: UserService) : BaseViewModel
             .onSuccess {
                 viewModelState.value = currentState.copy(userDetailsResult = SuccessResult(it))
             }
-            .onError {  }
+            .onError {
+                viewModelActionShowToast.value = Event(R.string.cant_load_user_details)
+                viewModelActionGoBack.value = Event(Unit)
+            }
             .autoCancel()
     }
 
@@ -42,11 +55,14 @@ class UserDetailsViewModel(private val userService: UserService) : BaseViewModel
         viewModelState.value = currentState.copy(deletingInProgress = true)
         userService.deleteUser(userDetailsResult.data.user)
             .onSuccess {
-                //todo
+                viewModelActionShowToast.value = Event(R.string.user_has_been_deleted)
+                viewModelActionGoBack.value = Event(Unit)
             }
             .onError {
                 viewModelState.value = currentState.copy(deletingInProgress = false)
+                viewModelActionShowToast.value = Event(R.string.cant_delete_user)
             }
+            .autoCancel()
     }
 
 
