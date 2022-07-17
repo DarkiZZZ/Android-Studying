94fe94f6c4c2a8f8e66ad62910970412088c3422
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-17 22:27:54 +0300

    added BaseViewModel with autoCancel while viewModel destroyed

--- BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/BaseViewModel.kt
+++ BasicGlideRecyclerViewTestApp/app/src/main/java/com/example/basicgliderecyclerviewtestapp/screens/BaseViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.basicgliderecyclerviewtestapp.screens
 
 import androidx.lifecycle.ViewModel
+import com.example.basicgliderecyclerviewtestapp.tasks.UserTask
 
-class BaseViewModel : ViewModel() {
+open class BaseViewModel : ViewModel() {
+
+    private val tasks = mutableListOf<UserTask<*>>()
+
+    override fun onCleared() {
+        super.onCleared()
+        tasks.forEach{it.cancel()}
+    }
+
+    fun <T> UserTask<T>.autoCancel(){
+        tasks.add(this)
+    }
 }
