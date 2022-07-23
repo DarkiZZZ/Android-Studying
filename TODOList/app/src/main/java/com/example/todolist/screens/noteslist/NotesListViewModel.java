e045a39e9e17c2fb17942646a2b15e180034b827
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-23 16:55:43 +0300

    added liveData to ViewModel

--- TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListViewModel.java
+++ TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListViewModel.java
@@ -1,4 +1,18 @@
 package com.example.todolist.screens.noteslist;
 
-public class NotesListViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.todolist.App;
+import com.example.todolist.model.Note;
+
+import java.util.List;
+
+public class NotesListViewModel extends ViewModel {
+
+    private LiveData<List<Note>> noteLiveData = App.getInstance().getNoteDao().getAllWithLiveData();
+
+    public LiveData<List<Note>> getNoteLiveData(){
+        return noteLiveData;
+    }
 }
