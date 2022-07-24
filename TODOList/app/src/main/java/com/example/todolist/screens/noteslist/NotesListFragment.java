edaa050965faa26c91d7c7563d074468f84717ba
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-24 11:58:57 +0300

    restructured project

--- TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListFragment.java
+++ TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListFragment.java
@@ -7,9 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.todolist.R;
 import com.example.todolist.databinding.FragmentNotesListBinding;
 
 public class NotesListFragment extends Fragment {
@@ -30,13 +27,6 @@ public class NotesListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(NotesListFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
     }
 
     @Override
