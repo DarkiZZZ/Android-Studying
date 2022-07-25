3e50da9d6c97286d9796b1fbd85b91cf26e10cd8
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-25 11:26:01 +0300

    added listeners and saveStates

--- TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListFragment.java
+++ TODOList/app/src/main/java/com/example/todolist/screens/noteslist/NotesListFragment.java
@@ -4,22 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todolist.R;
 import com.example.todolist.databinding.FragmentNotesListBinding;
+import com.example.todolist.model.Note;
+import com.example.todolist.screens.details.NotesDetailsFragment;
+
+import java.util.List;
 
 public class NotesListFragment extends Fragment {
 
     private FragmentNotesListBinding binding;
+    Adapter adapter = new Adapter();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentNotesListBinding.inflate(inflater, container, false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        binding.recyclerView.setLayoutManager(linearLayoutManager);
+        binding.recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
+
+        binding.recyclerView.setAdapter(adapter);
+
+        binding.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotesDetailsFragment.startFragment(new NotesListFragment(), null);
+            }
+        });
+
+        NotesListViewModel viewModel = ViewModelProviders.of(this).get(NotesListViewModel.class);
+        viewModel.getNoteLiveData().observe(getViewLifecycleOwner(), new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                adapter.setItems(notes);
+            }
+        });
         return binding.getRoot();
 
     }
