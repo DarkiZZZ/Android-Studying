6429bf33c0fedd51967f05d0c3f6761e8bc18469
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-25 09:45:06 +0300

    changed access modifier

--- TODOList/app/src/main/java/com/example/todolist/model/Note.java
+++ TODOList/app/src/main/java/com/example/todolist/model/Note.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 public class Note implements Parcelable {
 
     @PrimaryKey(autoGenerate = true)
-    private int uId;
+    public int uId;
 
     @ColumnInfo(name = "text")
     public String text;
