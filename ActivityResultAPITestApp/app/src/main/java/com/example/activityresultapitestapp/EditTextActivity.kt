2360cc0dc894ee41d94a2fa00b471c7d9bcbe40f
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-26 12:19:38 +0300

    added contract for EditTextActivity to get permission for intents

--- ActivityResultAPITestApp/app/src/main/java/com/example/activityresultapitestapp/EditTextActivity.kt
+++ ActivityResultAPITestApp/app/src/main/java/com/example/activityresultapitestapp/EditTextActivity.kt
@@ -1,17 +1,49 @@
 package com.example.activityresultapitestapp
 
+import android.content.Context
+import android.content.Intent
+import android.icu.util.Output
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
 import com.example.activityresultapitestapp.databinding.ActivityEditTextBinding
 
 class EditTextActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditTextBinding
 
+    data class Output(
+        val message: String,
+        val confirmed: Boolean
+    )
+
+    class Contract : ActivityResultContract<String, Output>() {
+
+        override fun createIntent(context: Context, input: String?): Intent =
+            Intent(context, EditTextActivity::class.java).apply {
+                putExtra(EXTRA_INPUT_MESSAGE, input)
+            }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Output? {
+            if (intent == null) return null
+            val message = intent.getStringExtra(EXTRA_OUTPUT_MESSAGE) ?: return null
+
+            val confirmed = resultCode == RESULT_OK
+            return Output(message, confirmed)
+        }
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityEditTextBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    companion object{
+        private const val EXTRA_INPUT_MESSAGE = "EXTRA_MESSAGE"
+        private const val EXTRA_OUTPUT_MESSAGE = "EXTRA_MESSAGE"
     }
 }
