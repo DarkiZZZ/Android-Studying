09b692bc7f766c60584e8adf987ac3fb3aef76c6
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-07-26 12:35:52 +0300

    added permission launcher for FINE_LOCATION, edit text in other activity, listeners

--- ActivityResultAPITestApp/app/src/main/java/com/example/activityresultapitestapp/MainActivity.kt
+++ ActivityResultAPITestApp/app/src/main/java/com/example/activityresultapitestapp/MainActivity.kt
@@ -2,16 +2,40 @@ package com.example.activityresultapitestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.activityresultapitestapp.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        Log.d(MainActivity::class.java.simpleName, "Permission granted: $it")
+    }
+
+    private val editMessageLauncher = registerForActivityResult(EditTextActivity.Contract()){
+        Log.d(MainActivity::class.java.simpleName, "Edit result: $it")
+        if (it.confirmed){
+            binding.testTextView.text = it.message
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.requestPermissionButton.setOnClickListener{ requestPermission()}
+        binding.editTextButton.setOnClickListener { editMessage() }
+    }
+
+    private fun editMessage() {
+        editMessageLauncher.launch(binding.testTextView.text.toString())
+    }
+
+    private fun requestPermission() {
+        requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
