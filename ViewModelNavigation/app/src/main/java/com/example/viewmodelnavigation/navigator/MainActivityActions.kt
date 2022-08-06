f1e0da7be63a1bb4224947ff1d826aacdd640c2c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-06 16:00:13 +0300

    filled class of main activity actions

--- ViewModelNavigation/app/src/main/java/com/example/viewmodelnavigation/navigator/MainActivityActions.kt
+++ ViewModelNavigation/app/src/main/java/com/example/viewmodelnavigation/navigator/MainActivityActions.kt
@@ -1,4 +1,33 @@
 package com.example.viewmodelnavigation.navigator
 
+import com.example.viewmodelnavigation.MainActivity
+
+typealias MainActivityAction = (MainActivity) -> Unit
+
 class MainActivityActions {
+
+    var mainActivity: MainActivity? = null
+    set(activity) {
+        field = activity
+        if (activity != null){
+            actions.forEach { it(activity) }
+            actions.clear()
+        }
+    }
+
+    operator fun invoke(action: MainActivityAction){
+        val activity = this.mainActivity
+        if (activity == null){
+            actions += action
+        }
+        else{
+            action(activity)
+        }
+    }
+
+    private val actions = mutableListOf<MainActivityAction>()
+
+    fun clear(){
+        actions.clear()
+    }
 }
