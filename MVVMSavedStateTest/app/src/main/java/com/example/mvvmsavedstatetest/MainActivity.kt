da3966e4d7bc71c891d958e7b96690d82024210d
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-08 08:45:37 +0300

    prepared activity for using fragment

--- MVVMSavedStateTest/app/src/main/java/com/example/mvvmsavedstatetest/MainActivity.kt
+++ MVVMSavedStateTest/app/src/main/java/com/example/mvvmsavedstatetest/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mvvmsavedstatetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmsavedstatetest.databinding.FragmentContainerBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: FragmentContainerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = FragmentContainerBinding.inflate(layoutInflater).also {
+            setContentView(binding.root)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, MainFragment())
+            .commit()
     }
 }
