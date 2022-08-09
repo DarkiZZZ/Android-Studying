6fdc167c1e9b1ed4b629dc016d7720a41ad1a0fd
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-09 15:45:31 +0300

    init commit

---
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/changecolor/ChangeColorFragment.kt
@@ -0,0 +1,4 @@
+package com.example.basedframemvvm.views.base.changecolor
+
+class ChangeColorFragment {
+}
---
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/currentcolor/CurrentColorViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.basedframemvvm.views.base.currentcolor
+
+class CurrentColorViewModel {
+}
