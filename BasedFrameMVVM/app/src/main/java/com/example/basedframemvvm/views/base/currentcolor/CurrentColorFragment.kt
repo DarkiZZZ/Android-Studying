5891f3fb88619cba7c67f47c92bd3a5c463afb41
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-09 16:38:09 +0300

    filled viemodels and fragments

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/currentcolor/CurrentColorFragment.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/currentcolor/CurrentColorFragment.kt
@@ -1,14 +1,35 @@
 package com.example.basedframemvvm.views.base.currentcolor
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.basedframemvvm.databinding.FragmentCurrentColorBinding
 import com.example.basedframemvvm.views.base.BaseFragment
 import com.example.basedframemvvm.views.base.BaseScreen
 import com.example.basedframemvvm.views.base.BaseViewModel
+import com.example.basedframemvvm.views.base.screenViewModel
 
 class CurrentColorFragment : BaseFragment() {
-    class Screen : BaseScreen {
 
+    // no arguments for this screen
+    class Screen : BaseScreen
+
+    override val viewModel by screenViewModel<CurrentColorViewModel>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentCurrentColorBinding.inflate(inflater, container, false)
+
+        viewModel.currentColor.observe(viewLifecycleOwner) {
+            binding.colorView.setBackgroundColor(it.value)
+        }
+
+        binding.changeColorButton.setOnClickListener {
+            viewModel.changeColor()
+        }
+
+        return binding.root
     }
 
-    override val viewModel: BaseViewModel
-        get() = TODO("Not yet implemented")
+
 }
