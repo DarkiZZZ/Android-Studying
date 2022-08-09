c314d9603c5d02c5f00aee621e258339d36a8989
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-09 15:51:08 +0300

    added initial Screen

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/currentcolor/CurrentColorFragment.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/base/currentcolor/CurrentColorFragment.kt
@@ -1,4 +1,14 @@
 package com.example.basedframemvvm.views.base.currentcolor
 
-class CurrentColorFragment {
+import com.example.basedframemvvm.views.base.BaseFragment
+import com.example.basedframemvvm.views.base.BaseScreen
+import com.example.basedframemvvm.views.base.BaseViewModel
+
+class CurrentColorFragment : BaseFragment() {
+    class Screen : BaseScreen {
+
+    }
+
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
 }
