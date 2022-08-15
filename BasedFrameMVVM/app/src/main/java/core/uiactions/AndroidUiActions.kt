9cc75e31958c37d62079b637388fdef822583b99
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-15 19:55:13 +0300

    removing MainViewModel, separate implementations into different smaller classes. This class for UiActions implementation

---
+++ BasedFrameMVVM/app/src/main/java/core/uiactions/AndroidUiActions.kt
@@ -0,0 +1,17 @@
+package core.uiactions
+
+import android.content.Context
+import android.widget.Toast
+import com.example.basedframemvvm.App
+
+class AndroidUiActions(
+    private val appContext: Context): UiActions {
+
+    override fun toast(message: String) {
+        Toast.makeText(appContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun getString(messageRes: Int, vararg args: Any): String {
+        return appContext.getString(messageRes, *args)
+    }
+}
