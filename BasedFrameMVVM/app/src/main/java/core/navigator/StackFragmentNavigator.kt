48ed2beb9b1f075c3dc79d9d48f5424a4a2a4362
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-15 19:55:28 +0300

    init commit

---
+++ BasedFrameMVVM/app/src/main/java/core/navigator/StackFragmentNavigator.kt
@@ -0,0 +1,13 @@
+package core.navigator
+
+import core.views.BaseScreen
+
+class StackFragmentNavigator: Navigator {
+    override fun launch(screen: BaseScreen) {
+        TODO("Not yet implemented")
+    }
+
+    override fun goBack(result: Any?) {
+        TODO("Not yet implemented")
+    }
+}
