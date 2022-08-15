8fbcf6f54cfde81729780d28fb3af2da8a0c34be
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-15 20:54:08 +0300

    added navigator implementation for activity

--- BasedFrameMVVM/app/src/main/java/core/navigator/StackFragmentNavigator.kt
+++ BasedFrameMVVM/app/src/main/java/core/navigator/StackFragmentNavigator.kt
@@ -1,13 +1,104 @@
 package core.navigator
 
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.basedframemvvm.R
+import com.example.basedframemvvm.views.currentcolor.CurrentColorFragment
+import core.ARG_SCREEN
+import core.utils.Event
+import core.views.BaseFragment
 import core.views.BaseScreen
+import core.views.HasScreenTitle
 
-class StackFragmentNavigator: Navigator {
-    override fun launch(screen: BaseScreen) {
-        TODO("Not yet implemented")
+class StackFragmentNavigator(
+    private val activity: AppCompatActivity,
+    @IdRes private val containerId: Int,
+    private val initialScreenCreator: () -> BaseScreen
+): Navigator {
+
+    override fun launch(screen: BaseScreen){
+        launchFragment(screen)
     }
 
     override fun goBack(result: Any?) {
-        TODO("Not yet implemented")
+        if (result != null) {
+            this.result = Event(result)
+        }
+        activity.onBackPressed()
+    }
+
+
+    private var result : Event<Any>? = null
+
+    fun onCreate(savedInstanceState: Bundle?){
+        if (savedInstanceState == null){
+            launchFragment(
+                screen = CurrentColorFragment.Screen(),
+                addToBackStack = false
+            )
+        }
+        activity.supportFragmentManager.
+            registerFragmentLifecycleCallbacks(fragmentCallbacks, false)
+    }
+
+    fun onDestroy(){
+        activity.supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentCallbacks)
+    }
+
+    fun launchFragment(screen: BaseScreen, addToBackStack: Boolean = true) {
+        val fragment = screen.javaClass.enclosingClass.newInstance() as Fragment
+        fragment.arguments = bundleOf(ARG_SCREEN to screen)
+
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        if (addToBackStack) transaction.addToBackStack(null)
+        transaction
+        .setCustomAnimations(
+        R.anim.enter,
+        R.anim.exit,
+        R.anim.pop_enter,
+        R.anim.pop_exit
+        )
+        .replace(R.id.fragmentContainer,fragment)
+            .commit()
+    }
+
+    fun notifyScreenUpdates(){
+        val fragment = activity.supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+
+        if (activity.supportFragmentManager.backStackEntryCount > 0){
+            activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        else{
+            activity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        }
+
+        if (fragment is HasScreenTitle && fragment.getScreenTitle() != null){
+            activity.supportActionBar?.title = fragment.getScreenTitle()
+        }
+        else{
+            activity.supportActionBar?.title = activity.getString(R.string.app_name)
+        }
+    }
+
+    private fun publishResults(fragment: Fragment){
+        val result = result?.getValue() ?: return
+        if (fragment is BaseFragment) {
+            fragment.viewModel.onResult(result)
+        }
+    }
+
+    private val fragmentCallbacks = object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentCreated(
+            fragmentManager: FragmentManager,
+            fragment: Fragment,
+            savedInstanceState: Bundle?
+        ) {
+            notifyScreenUpdates()
+            publishResults(fragment)
+        }
     }
 }
