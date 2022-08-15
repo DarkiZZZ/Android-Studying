40b404d0aa25730bb2b4a3ee247bda0a54d90976
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-15 21:57:00 +0300

    moved inteface implementation to proper classes

--- BasedFrameMVVM/app/src/main/java/core/ActivityScopeViewModel.kt
+++ BasedFrameMVVM/app/src/main/java/core/ActivityScopeViewModel.kt
@@ -20,54 +20,18 @@ import core.views.MutableLiveEvent
 
 const val ARG_SCREEN = "ARG_SCREEN"
 
-class MainViewModel(
+class ActivityScopeViewModel(
     val uiActions: UiActions,
     val navigator: IntermediateNavigator
 ): ViewModel(), Navigator by navigator, UiActions by uiActions{
 
     val whenActivityActive = ResourceActions<MainActivity>()
 
-    private val _result = MutableLiveEvent<Any>()
-    val result: LiveEvent<Any> = _result
 
-
-
-   /* override fun launch(screen: BaseScreen) = whenActivityActive{
-        launchFragment(it, screen)
-    }
-
-    override fun goBack(result: Any?) = whenActivityActive {
-        if (result != null) {
-            _result.value = Event(result)
-        }
-        it.onBackPressed()
-    }
-
-    fun launchFragment(activity: MainActivity, screen: BaseScreen, addToBackStack: Boolean = true) {
-        val fragment = screen.javaClass.enclosingClass.newInstance() as Fragment
-        fragment.arguments = bundleOf(ARG_SCREEN to screen)
-
-        val transaction = activity.supportFragmentManager.beginTransaction()
-        if (addToBackStack) transaction.addToBackStack(null)
-        transaction
-            *//*.setCustomAnimations(
-                //todo,
-                //todo,
-                //todo,
-                //todo
-            )*//*
-            .replace(R.id.fragmentContainer,fragment)
-            .commit()
-    }*/
 
     override fun onCleared() {
         super.onCleared()
         navigator.clear()
     }
-
-    /*companion object{
-        private const val ARG_SCREEN = "ARG_SCREEN"
-    }*/
-
 
 }
