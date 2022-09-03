db1ebd8fb26b11ae999a7f554a12f040047606a4
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-04 00:34:18 +0300

    synchronized methods for multithreading

--- BasedFrameMVVM/app/src/main/java/core/model/tasks/SynchronizedTask.kt
+++ BasedFrameMVVM/app/src/main/java/core/model/tasks/SynchronizedTask.kt
@@ -2,31 +2,45 @@ package core.model.tasks
 
 import core.model.tasks.dispatchers.Dispatcher
 import java.lang.IllegalStateException
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SynchronizedTask<T>(
     private val task: Task<T>
 ): Task<T> {
 
+    @Volatile
     private var cancelled = false
+
     private var executed = false
+    private var listenerCalled = AtomicBoolean(false)
 
     override fun await(): T {
-        if (cancelled) throw CancelledException()
-        if (executed) throw IllegalStateException("Task has been executed")
+        synchronized(this){
+            if (cancelled) throw CancelledException()
+            if (executed) throw IllegalStateException("Task has been executed")
+            executed = true
+        }
         return task.await()
     }
 
-    override fun cancel() {
-        if (cancelled) return
-        cancelled = true
-        task.cancel()
+    override fun cancel() = synchronized(this) {
+        if (listenerCalled.compareAndSet(false, true)){
+            if (cancelled) return
+            cancelled = true
+            task.cancel()
+        }
     }
 
-    override fun enqueue(dispatcher: Dispatcher, listener: TaskListener<T>) {
+    override fun enqueue(dispatcher: Dispatcher, listener: TaskListener<T>) = synchronized(this) {
         if (cancelled) return
         if (executed) throw IllegalStateException("Task has been executed")
         executed = true
-        task.enqueue(dispatcher, listener)
+        val finalListener: TaskListener<T> = {result ->
+            if (listenerCalled.compareAndSet(false, true)){
+                if (!cancelled) listener(result)
+            }
+        }
+        task.enqueue(dispatcher, finalListener)
     }
 
 }
