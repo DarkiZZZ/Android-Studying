a32b9c9449321ddbc2679e0abd46d9eee346d7c5
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-04 14:38:15 +0300

    added tasks factories for testing

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/App.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/App.kt
@@ -5,17 +5,26 @@ import com.example.basedframemvvm.model.colors.InMemoryColorsRepository
 import core.BaseApplication
 import core.model.tasks.ThreadUtils
 import core.model.tasks.dispatchers.MainThreadDispatcher
+import core.model.tasks.factories.ExecutorServiceTasksFactory
+import core.model.tasks.factories.HandlerThreadTasksFactory
 import core.model.tasks.factories.ThreadTasksFactory
+import java.util.concurrent.Executors
 
 class App : Application(), BaseApplication {
 
+    /**Testing Tasks factories:
     private val tasksFactory = ThreadTasksFactory()
+    private val singleThreadPoolExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newSingleThreadExecutor())**/
+    private val cachedThreadPoolExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newCachedThreadPool())
+    private val handlerThreadTasksFactory = HandlerThreadTasksFactory()
+
     private val threadUtils = ThreadUtils.Default()
     private val dispatcher = MainThreadDispatcher()
 
+
     override val singletonScopeDependencies: List<Any> = listOf(
-        tasksFactory,
+        cachedThreadPoolExecutorTasksFactory,
         dispatcher,
-        InMemoryColorsRepository(tasksFactory, threadUtils)
+        InMemoryColorsRepository(handlerThreadTasksFactory, threadUtils)
     )
 }
