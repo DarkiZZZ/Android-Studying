943b8384ec57e3089e7f750b8f9ed238f0b69d3a
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-04 13:34:51 +0300

    fixed unsafe multithreading

--- BasedFrameMVVM/app/src/main/java/core/utils/delegates/Await.kt
+++ BasedFrameMVVM/app/src/main/java/core/utils/delegates/Await.kt
@@ -1,21 +1,22 @@
 package core.utils.delegates
 
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.reflect.KProperty
 
 class Await<T> {
 
     private val countDownLatch = CountDownLatch(1)
-    var value: T? = null
+    private var value = AtomicReference<T>(null)
 
     operator fun getValue(thisRef: Any, property: KProperty<*>): T{
         countDownLatch.await()
-        return value!!
+        return value.get()
     }
 
     operator fun setValue(thisRef: Any, property: KProperty<*>, value: T){
-        if (this.value == null && value != null){
-            this.value = value
+        if (value == null) return
+        if (this.value.compareAndSet(null, value)){
             countDownLatch.countDown()
         }
     }
