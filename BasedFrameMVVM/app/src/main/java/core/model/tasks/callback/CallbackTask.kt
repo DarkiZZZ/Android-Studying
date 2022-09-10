2cec2014c856433675d472267f730fb03ecbea8b
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-10 22:46:15 +0300

    updated callback and emitter

--- BasedFrameMVVM/app/src/main/java/core/model/tasks/callback/CallbackTask.kt
+++ BasedFrameMVVM/app/src/main/java/core/model/tasks/callback/CallbackTask.kt
@@ -1,19 +1,43 @@
 package core.model.tasks.callback
 
+import core.model.FinalResult
 import core.model.tasks.AbstractTask
+import core.model.tasks.SynchronizedTask
+import core.model.tasks.Task
 import core.model.tasks.TaskListener
 
 class CallbackTask<T> private constructor(
     private val executionListener: ExecutionListener<T>
-): AbstractTask<T>(){
+): AbstractTask<T>() {
 
-    private val emitter: EmitterImpl<T>? = null
-
-    override fun doCancel() {
-        TODO("Not yet implemented")
-    }
+    private var emitter: EmitterImpl<T>? = null
 
     override fun doEnqueue(listener: TaskListener<T>) {
-        emitter = EmitterImpl(listener).also{executionListener(it)}
+        emitter = EmitterImpl(listener).also { executionListener(it) }
     }
+
+    override fun doCancel() {
+        emitter?.onCancelListener?.invoke()
+    }
+
+    companion object {
+        fun <T> create(executionListener: ExecutionListener<T>): Task<T> {
+            return SynchronizedTask(CallbackTask(executionListener))
+        }
+    }
+
+    private class EmitterImpl<T>(
+        private val taskListener: TaskListener<T>
+    ) : Emitter<T> {
+        var onCancelListener: CancelListener? = null
+
+        override fun emit(finalResult: FinalResult<T>) {
+            taskListener.invoke(finalResult)
+        }
+
+        override fun setCancelListener(cancelListener: CancelListener) {
+            this.onCancelListener = cancelListener
+        }
+    }
+
 }
