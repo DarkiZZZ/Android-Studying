b19ad7af64426398f19a6e7065a773344bf6d189
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-11 22:20:31 +0300

    got rid of dispatchers and tasks, so deleted factories in App.kt

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/App.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/App.kt
@@ -3,28 +3,10 @@ package com.example.basedframemvvm
 import android.app.Application
 import com.example.basedframemvvm.model.colors.InMemoryColorsRepository
 import core.BaseApplication
-import core.model.tasks.ThreadUtils
-import core.model.tasks.dispatchers.MainThreadDispatcher
-import core.model.tasks.factories.ExecutorServiceTasksFactory
-import core.model.tasks.factories.HandlerThreadTasksFactory
-import core.model.tasks.factories.ThreadTasksFactory
-import java.util.concurrent.Executors
 
 class App : Application(), BaseApplication {
 
-    /**Testing Tasks factories:
-    private val tasksFactory = ThreadTasksFactory()
-    private val singleThreadPoolExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newSingleThreadExecutor())**/
-    private val cachedThreadPoolExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newCachedThreadPool())
-    private val handlerThreadTasksFactory = HandlerThreadTasksFactory()
-
-    private val threadUtils = ThreadUtils.Default()
-    private val dispatcher = MainThreadDispatcher()
-
-
     override val singletonScopeDependencies: List<Any> = listOf(
-        cachedThreadPoolExecutorTasksFactory,
-        dispatcher,
-        InMemoryColorsRepository(handlerThreadTasksFactory, threadUtils)
+        InMemoryColorsRepository()
     )
 }
