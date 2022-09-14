4471ab3b6aca8023393c847a780360e8ec559ad4
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-15 00:12:43 +0300

    removed extension method for observing state flow

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/changecolor/ChangeColorFragment.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/views/changecolor/ChangeColorFragment.kt
@@ -5,20 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.basedframemvvm.R
 import com.example.basedframemvvm.databinding.FragmentChangeColorBinding
+import com.example.basedframemvvm.views.collectFlow
 import com.example.basedframemvvm.views.onTryAgain
 import com.example.basedframemvvm.views.renderSimpleResult
 import core.views.HasScreenTitle
 import core.views.BaseFragment
 import core.views.BaseScreen
 import core.views.screenViewModel
-import kotlinx.coroutines.launch
-
 class ChangeColorFragment : BaseFragment(), HasScreenTitle {
 
     /**
@@ -45,19 +41,18 @@ class ChangeColorFragment : BaseFragment(), HasScreenTitle {
         binding.saveButton.setOnClickListener { viewModel.onSavePressed() }
         binding.cancelButton.setOnClickListener { viewModel.onCancelPressed() }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.viewState.collect { result ->
-                    renderSimpleResult(binding.root, result){ viewState ->
-                        adapter.items = viewState.colorsList
-                        binding.saveButton.visibility = if (viewState.showSaveButton) View.VISIBLE else View.INVISIBLE
+        collectFlow(viewModel.viewState){ result ->
+            renderSimpleResult(binding.root, result){ viewState ->
+                adapter.items = viewState.colorsList
+                binding.saveButton.visibility = if (viewState.showSaveButton) View.VISIBLE else View.INVISIBLE
 
-                        binding.cancelButton.visibility = if (viewState.showCancelButton) View.VISIBLE else View.INVISIBLE
+                binding.cancelButton.visibility = if (viewState.showCancelButton) View.VISIBLE else View.INVISIBLE
 
-                        binding.saveProgressBar.visibility = if (viewState.showSaveProgressBar) View.VISIBLE else View.GONE
-                    }
-                }
+                binding.saveProgressGroup.visibility = if (viewState.showSaveProgressBar) View.VISIBLE else View.GONE
+                binding.saveProgressBar.progress = viewState.saveProgressPercentage
+                binding.savingPercentageTextView.text = viewState.saveProgressPercentageMessage
             }
+
         }
 
         viewModel.screenTitle.observe(viewLifecycleOwner) {
