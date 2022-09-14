8f60a3c60315ff0092e388fb19c843a506c79e19
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-14 20:36:45 +0300

    replaced removing and adding listeners with sharing flow

--- BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/model/colors/ColorsRepository.kt
+++ BasedFrameMVVM/app/src/main/java/com/example/basedframemvvm/model/colors/ColorsRepository.kt
@@ -1,6 +1,7 @@
 package com.example.basedframemvvm.model.colors
 
 import core.model.Repository
+import kotlinx.coroutines.flow.Flow
 
 typealias ColorListener = (NamedColor) -> Unit
 
@@ -9,13 +10,11 @@ interface ColorsRepository: Repository {
 
     suspend fun getCurrentColor(): NamedColor // Get current color/
 
-    suspend fun setCurrentColor(color: NamedColor) // Set current color as current/
+    suspend fun setCurrentColor(color: NamedColor): Flow<Int> // Set current color as current/
 
     suspend fun getAvailableColors(): List<NamedColor> // Get list of all available color that may be chosen by user.
 
     suspend fun getById(id: Long) : NamedColor // Get the color content by its ID
 
-    fun addListener(listener: ColorListener) // Listener is triggered immediately with current value when calling this method
-
-    fun removeListener(listener: ColorListener)
+    fun listenToCurrentColor(): Flow<NamedColor>
 }
