1b387bcb431778f15d84722fc8b6620eace495b0
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-21 23:55:03 +0300

    added methods to work with permissions, added extra code to work with location permissions above 12 android

--- PermissionsTest/app/src/main/java/com/example/permissionstest/MainActivity.kt
+++ PermissionsTest/app/src/main/java/com/example/permissionstest/MainActivity.kt
@@ -1,43 +1,59 @@
 package com.example.permissionstest
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.permissionstest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d("TAG", "onCreate started")
 
         binding.requestPermissionOneButton.setOnClickListener {
+            Log.d("TAG", "Camera request")
             checkAndRequestPermission(REQUEST_CODE_CAMERA, Manifest.permission.CAMERA)
         }
+
         binding.requestPermissionTwoButton.setOnClickListener {
+            Log.d("TAG", "Audio request")
             checkAndRequestPermission(REQUEST_CODE_AUDIO, Manifest.permission.RECORD_AUDIO)
         }
-        binding.requestPermissionThreeButton.setOnClickListener {
-            checkAndRequestPermission(REQUEST_CODE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-    }
 
-    private fun checkAndRequestPermission(requestCode: Int, vararg permissions: String ){
-        if (ContextCompat.checkSelfPermission(this, permissions[0]) ==
+        binding.requestPermissionThreeButton.setOnClickListener {
+            Log.d("TAG", "Location request")
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED){
-            //while permission has been GRANTED already todo something
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(*permissions),
-                requestCode
-            )
+                showToast(R.string.permission_granted)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION),
+                    REQUEST_CODE_LOCATION
+                )
+            }
         }
+
+        binding.testButton.setOnClickListener {
+            Log.d("TAG", "test")
+            Toast.makeText(this, "Test!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onRequestPermissionsResult(
@@ -47,34 +63,98 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            REQUEST_CODE_CAMERA -> {}
+            REQUEST_CODE_CAMERA -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    onPermissionGrantedMessageShow(REQUEST_CODE_CAMERA)
+                }
+                else{
+                    if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                        askUserForOpeningAppSettings()
+                    } else{
+                        showToast(R.string.permission_denied_message)
+                    }
+                }
+            }
 
-            REQUEST_CODE_AUDIO -> {}
+            REQUEST_CODE_AUDIO -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    onPermissionGrantedMessageShow(REQUEST_CODE_AUDIO)
+                }
+                else{
+                    if (!shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)){
+                        askUserForOpeningAppSettings()
+                    } else{
+                        showToast(R.string.permission_denied_message)
+                    }
+                }
+            }
 
             REQUEST_CODE_LOCATION -> {
-                // If the request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty()) {
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        // ACCESS_FINE_LOCATION is granted
-                    } else if (grantResults[1] ==
-                        PackageManager.PERMISSION_GRANTED) {
-                        // ACCESS_COARSE_LOCATION is granted
+                Log.d("TAG", "grantResult are NOT empty")
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showToast(R.string.fine_location_permission_granted_message)
+                } else if (grantResults[1] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                        showToast(R.string.coarse_location_permission_granted_message)
+                } else{
+                    if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                        !shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)){
+                        askUserForOpeningAppSettings()
+                    } else{
+                        showToast(R.string.permission_denied_message)
                     }
-                } else {
-                    // Explain to the user that the feature is unavailable because
-                    // the feature requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
                 }
-                return
             }
+        }
+    }
 
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
+    private fun checkAndRequestPermission(requestCode: Int, vararg permissions: String ){
+        if (ContextCompat.checkSelfPermission(this, permissions[0]) ==
+            PackageManager.PERMISSION_GRANTED){
+            showToast(R.string.permission_granted)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                permissions.toList().toTypedArray(),
+                requestCode
+            )
+        }
+    }
+
+    private fun onPermissionGrantedMessageShow(requestCode: Int){
+        when(requestCode){
+            REQUEST_CODE_CAMERA -> showToast(R.string.camera_permission_granted)
+            REQUEST_CODE_AUDIO ->  showToast(R.string.audio_permission_granted)
+            REQUEST_CODE_LOCATION -> showToast(R.string.location_permission_granted)
+        }
+    }
+
+    private fun showToast(message: Int){
+        Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun askUserForOpeningAppSettings(){
+        val appSettingsIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        if (packageManager.resolveActivity(appSettingsIntent, PackageManager.MATCH_DEFAULT_ONLY) == null){
+            Toast.makeText(this, getString(R.string.permission_denied_forever_message), Toast.LENGTH_SHORT).show()
+        }
+        else{
+            AlertDialog.Builder(this)
+                .setTitle("Permissions denied")
+                .setMessage("You have denied permissions forever." +
+                            "You can change your decision in app settings.\n\n" +
+                            "Would you like to open app settings?")
+                .setPositiveButton("Open"){_, _ ->
+                    startActivity(appSettingsIntent)
+                }
+                .setNegativeButton("Close"){dialog, _ ->
+                    dialog.cancel()
+                }
+                .create()
+                .show()
         }
     }
 
