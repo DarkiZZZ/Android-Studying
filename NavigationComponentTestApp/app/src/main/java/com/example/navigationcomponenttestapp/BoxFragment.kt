7bb9a094457553f718ee6c4ecee1e15603252cef
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-09-27 14:25:15 +0300

    fixed not working app by using nav args

--- NavigationComponentTestApp/app/src/main/java/com/example/navigationcomponenttestapp/BoxFragment.kt
+++ NavigationComponentTestApp/app/src/main/java/com/example/navigationcomponenttestapp/BoxFragment.kt
@@ -1,6 +1,45 @@
 package com.example.navigationcomponenttestapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.navigationcomponenttestapp.databinding.FragmentBoxBinding
+import kotlin.random.Random
 
-class BoxFragment: Fragment() {
+class BoxFragment : Fragment(R.layout.fragment_box){
+
+    private lateinit var binding: FragmentBoxBinding
+
+    private val args: BoxFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBoxBinding.bind(view)
+
+        val color = args.color
+
+        binding.root.setBackgroundColor(color)
+
+        binding.goBackButton.setOnClickListener {
+            // just go back to the previous screen, also navigateUp() can be used
+            findNavController().popBackStack()
+        }
+        binding.openSecretButton.setOnClickListener {
+            // launch the next screen without any arguments
+            findNavController().navigate(BoxFragmentDirections.actionBoxFragmentToSecretFragment())
+        }
+        binding.generateNumberButton.setOnClickListener {
+            val number = Random.nextInt(100)
+            // send the result
+            publishResults(EXTRA_RANDOM_NUMBER, number)
+            // go back to the previous screen, also navigateUp() can be used
+            findNavController().popBackStack()
+        }
+    }
+
+    companion object {
+        const val EXTRA_RANDOM_NUMBER = "EXTRA_RANDOM_NUMBER"
+    }
 }
--- NavigationComponentTestApp/app/src/main/java/com/example/navigationcomponenttestapp/SecretFragment.kt
+++ NavigationComponentTestApp/app/src/main/java/com/example/navigationcomponenttestapp/SecretFragment.kt
@@ -1,6 +1,26 @@
 package com.example.navigationcomponenttestapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponenttestapp.databinding.FragmentSecretBinding
 
-class SecretFragment: Fragment() {
+class SecretFragment : Fragment(R.layout.fragment_secret) {
+
+    private lateinit var binding: FragmentSecretBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecretBinding.bind(view)
+        binding.closeBoxButton.setOnClickListener {
+            // go back to the specified destination
+            findNavController().popBackStack(R.id.rootFragment, false)
+        }
+        binding.goBackButton.setOnClickListener {
+            // go back to the previous screen
+            findNavController().popBackStack()
+        }
+    }
+
 }
