2a4885c8fbadc2088b7c36faf9c8e9ff55ecc5bb
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-27 23:58:27 +0300

    viewmodel initializing

--- JSONtestJava/app/src/main/java/ru/msokolov/jsontestjava/MainActivity.java
+++ JSONtestJava/app/src/main/java/ru/msokolov/jsontestjava/MainActivity.java
@@ -1,14 +1,35 @@
 package ru.msokolov.jsontestjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel viewModel;
+    private static final String TAG = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadImage();
+        viewModel.getDogImage().observe(this, dogImage -> {
+            Log.d(TAG, dogImage.toString());
+        });
     }
+
+
 }
