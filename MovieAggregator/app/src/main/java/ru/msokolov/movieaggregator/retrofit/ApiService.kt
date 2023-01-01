f725f02428cd4442cfcdea2cab9064218f7d4f09
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-01 22:47:58 +0300

    added parameter for handling page number

--- MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/ApiService.kt
+++ MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/ApiService.kt
@@ -2,10 +2,11 @@ package ru.msokolov.movieaggregator.retrofit
 
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 import ru.msokolov.movieaggregator.retrofit.entities.ApiResponse
 
 interface ApiService {
 
     @GET("movie?field=rating.kp&search=7-10&field=year&search=2017-2020&field=typeNumber&search=1&sortField=year&sortType=1&sortField=votes.imdb&sortType=-1&token=BCT6RGP-C3DMMCP-KE0PJ6K-VGYEQ7K")
-    fun loadMovies(): Single<ApiResponse>
+    fun loadMovies(@Query("page") page: Int): Single<ApiResponse>
 }
