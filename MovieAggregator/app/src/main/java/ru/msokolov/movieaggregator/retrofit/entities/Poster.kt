dc06b8a81a9a94af59f0458decfdf45ad02ae1b5
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-03 20:12:37 +0300

    added serializable for data classes

--- MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/entities/Poster.kt
+++ MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/entities/Poster.kt
@@ -1,8 +1,9 @@
 package ru.msokolov.movieaggregator.retrofit.entities
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Poster(
     @SerializedName("url")
     var url: String
-)
+) : Serializable
--- MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/entities/Rating.kt
+++ MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/retrofit/entities/Rating.kt
@@ -1,8 +1,9 @@
 package ru.msokolov.movieaggregator.retrofit.entities
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Rating(
     @SerializedName("kp")
     var kp: Double
-)
+) : Serializable
