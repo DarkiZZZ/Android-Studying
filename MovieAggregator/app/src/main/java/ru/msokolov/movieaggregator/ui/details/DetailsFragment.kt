ddaa178a5fce991feabb320a17c413966ff27760
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-05 12:54:33 +0300

    added method click to watch trailers in browser

--- MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/ui/details/DetailsFragment.kt
+++ MovieAggregator/app/src/main/java/ru/msokolov/movieaggregator/ui/details/DetailsFragment.kt
@@ -1,26 +1,19 @@
 package ru.msokolov.movieaggregator.ui.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.msokolov.movieaggregator.databinding.FragmentDetailsBinding
-import ru.msokolov.movieaggregator.retrofit.ApiFactory
-import ru.msokolov.movieaggregator.ui.MainActivity
 import ru.msokolov.movieaggregator.ui.details.adapter.TrailerAdapter
-import ru.msokolov.movieaggregator.ui.show.ShowFragmentDirections
-import ru.msokolov.movieaggregator.ui.show.adapter.MovieAdapter
 
 class DetailsFragment : Fragment() {
 
@@ -62,7 +55,9 @@ class DetailsFragment : Fragment() {
         binding.recyclerView.adapter = trailerAdapter
 
         trailerAdapter.onItemClickListener = { trailer ->
-            // TODO("intent to browser to watch trailer")
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(trailer.url)
+            startActivity(intent)
         }
     }
 }
