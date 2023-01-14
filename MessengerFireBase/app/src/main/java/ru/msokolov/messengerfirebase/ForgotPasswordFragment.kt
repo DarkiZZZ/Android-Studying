95d12c5b877119f4a9d444ddb2f3dbd9c77cb412
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-14 16:50:57 +0300

    added email argument from one fragment to another

--- MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/ForgotPasswordFragment.kt
+++ MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/ForgotPasswordFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import ru.msokolov.messengerfirebase.databinding.FragmentForgotPasswordBinding
 
 class ForgotPasswordFragment : Fragment() {
 
     private lateinit var binding: FragmentForgotPasswordBinding
+    private val args by navArgs<ForgotPasswordFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentForgotPasswordBinding.inflate(layoutInflater, container, false)
+        binding.emailEditText.setText(args.email)
         return binding.root
     }
 }
--- MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/SignInFragment.kt
+++ MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/SignInFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.msokolov.messengerfirebase.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
@@ -16,6 +17,19 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSignInBinding.inflate(layoutInflater, container, false)
+
+        binding.forgotPasswordTextView.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            if (email.isBlank()){
+                findNavController()
+                    .navigate(R.id.action_signInFragment_to_forgotPasswordFragment)
+            }
+            else{
+                findNavController()
+                    .navigate(SignInFragmentDirections
+                    .actionSignInFragmentToForgotPasswordFragment(email))
+            }
+        }
         return binding.root
     }
 }
