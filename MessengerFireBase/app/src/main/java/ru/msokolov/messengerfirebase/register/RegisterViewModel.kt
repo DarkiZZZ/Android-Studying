b8894102c63acfb4c2742ddee90ec9457c25c04b
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-17 00:41:16 +0300

    added full register method to firebase DB

--- MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/register/RegisterViewModel.kt
+++ MessengerFireBase/app/src/main/java/ru/msokolov/messengerfirebase/register/RegisterViewModel.kt
@@ -6,10 +6,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import ru.msokolov.messengerfirebase.User
 
 class RegisterViewModel : ViewModel(){
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val database = Firebase.database
+    private val usersDBRef = database.getReference("Users")
 
     private var _error: MutableLiveData<String> = MutableLiveData()
     val error: LiveData<String> = _error
@@ -25,6 +31,17 @@ class RegisterViewModel : ViewModel(){
     ){
         auth.createUserWithEmailAndPassword(email, password)
             .addOnSuccessListener { authResult ->
+                val firebaseUser = authResult.user ?: return@addOnSuccessListener
+                val id = firebaseUser.uid
+                val userToDB = User(
+                    id = id,
+                    firstName = firstName,
+                    lastName = lastName,
+                    age = age,
+                    isOnline = false
+                )
+                usersDBRef.child(id).setValue(userToDB)
+                //
                 _currentUser.value = authResult.user
             }
             .addOnFailureListener { exception ->
