9ab165b3fe75249e89c735b9ba4c9092a329ff3c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-01-21 10:53:18 +0300

    added viewBinging & filled main XML

--- SOLIDexample/app/src/main/java/ru/msokolov/solidexample/presentation/MainActivity.kt
+++ SOLIDexample/app/src/main/java/ru/msokolov/solidexample/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.msokolov.solidexample
+package ru.msokolov.solidexample.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.msokolov.solidexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
