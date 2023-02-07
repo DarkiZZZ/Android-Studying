1dfe926514bc2670e7c0b21c5635520ba4ac8864
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-07 19:17:10 +0300

    added permission handler and added code to handle camera preview

--- CameraXTest/app/src/main/java/ru/msokolov/cameraxtest/MainActivity.kt
+++ CameraXTest/app/src/main/java/ru/msokolov/cameraxtest/MainActivity.kt
@@ -1,17 +1,92 @@
 package ru.msokolov.cameraxtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import ru.msokolov.cameraxtest.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSION_LIST,
+                PERMISSION_CODE
+            )
+        }
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_CODE) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build()
+                    .also {
+                        it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                    }
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                } catch (e: Exception) {
+                    Log.e(TAG, "Camera provider bind error", e)
+                }
+            }, ContextCompat.getMainExecutor(this)
+        )
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return PERMISSION_LIST.all {
+            ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    companion object {
+        private const val TAG = "CameraX"
+        private const val FILE_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PERMISSION_CODE = 10
+        private val PERMISSION_LIST = arrayOf(android.Manifest.permission.CAMERA)
     }
 }
