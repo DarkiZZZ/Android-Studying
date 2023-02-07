14c86c486840d0569df5c541b9c3b366ad49fc92
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-07 17:52:51 +0300

    filled XML, added binding to MainActivity.kt

--- CameraXTest/app/src/main/java/ru/msokolov/cameraxtest/MainActivity.kt
+++ CameraXTest/app/src/main/java/ru/msokolov/cameraxtest/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.msokolov.cameraxtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.msokolov.cameraxtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
