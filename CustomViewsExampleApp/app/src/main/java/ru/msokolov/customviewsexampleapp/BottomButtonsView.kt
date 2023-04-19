6e984df1524ed919533364ab251ec245452f1d0c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-04-20 00:09:32 +0300

    added xml of custom view, filled constructor of class

---
+++ CustomViewsExampleApp/app/src/main/java/ru/msokolov/customviewsexampleapp/BottomButtonsView.kt
@@ -0,0 +1,17 @@
+package ru.msokolov.customviewsexampleapp
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class BottomButtonsView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+): ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context): this(context, null)
+}
